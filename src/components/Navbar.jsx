142f4938ee37cc74d0bffafe7153e13c9b896917
murillo oliveira <murillo1047@gmail.com>
2024-08-23 20:44:37 -0300

    feat: basic setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <header>
+        <img className="logo-header" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/1200px-Airbnb_Logo_B%C3%A9lo.svg.png" alt="Logo AirBnb" />
+        <ul className='header-buttons'>
+            <li>Qualquer lugar</li>
+            <li>Qualquer semana</li>
+            <li>
+                Hóspedes?
+                <span className="material-symbols-outlined search">search</span>
+            </li>
+        </ul>
+        <p>Anuncie seu espaço no Airbnb</p>
+        <span className="material-symbols-outlined">language</span>
+        <div>
+            <span className="material-symbols-outlined">menu</span>
+            <span className="material-symbols-outlined">account_circle</span>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
