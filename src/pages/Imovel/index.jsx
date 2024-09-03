07dbcd7cc08d2ef6f9ec56b934aab0f6e7243ae5
Yasmin Gonçalves <yasmingoncalves5421@gmail.com>
2024-09-03 20:14:24 -0300

    feat: grid images

--- src/pages/Imovel/index.jsx
+++ src/pages/Imovel/index.jsx
@@ -1,4 +1,5 @@
 import "./index.css"
+import { img1, img2, img3, img4, img5 } from "../../assets"
 
 const index = () => {
   return (
@@ -23,10 +24,18 @@ const index = () => {
         <h2>Espaço inteiro: casa em Chácara Inglesa, Brasil</h2>
         <p>6 hóspedes • 2 quartos • 1 cama • 2 banheiros e meio</p>
 
+        <section className="images">
+            <img className="img1" src={img1} alt="Imagem 1" />
+            <img className="img2" src={img2} alt="Imagem 2" />
+            <img className="img3" src={img3} alt="Imagem 3" />
+            <img className="img4" src={img4} alt="Imagem 4" />
+            <img className="img5" src={img5} alt="Imagem 5" />
+        </section>
+
         <section className="avaliações">
             <article className="card">
                 <div className="profile">
-                    <img src="https://a0.muscache.com/im/pictures/user/78cc5842-4314-43ad-8af4-46d34938c7b9.jpg?im_w=240" alt="mariana" />
+                    {/* <img src="https://a0.muscache.com/im/pictures/user/78cc5842-4314-43ad-8af4-46d34938c7b9.jpg?im_w=240" alt="mariana" /> */}
                     <div className="profile-name">
                         <h3>Mariana</h3>
                         <p>3 anos no Airbnb</p>
