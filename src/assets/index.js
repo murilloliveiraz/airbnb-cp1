7dccff386de68807c7849d432c0cf678a735c9bc
Murillo Oliveira <murillo1047@gmail.com>
2024-09-05 19:57:00 -0300

    merge: merge with new features

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,16 +3,13 @@ import img2 from './img2.png'
 import img3 from './img3.png'
 import img4 from './img4.png'
 import img5 from './img5.png'
+import star from "./black-star-icon.svg"
 
 export {
     img1,
     img2,
     img3,
     img4,
-    img5
-}
-import star from "./black-star-icon.svg"
-
-export{
+    img5,
     star
 }
