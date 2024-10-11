6ce6ea3656e5361a08801a9f31e8062dab787268
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-11 19:44:55 -0300

    feat: feattt

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,10 @@ import './Navbar.css';
 
 const Navbar = () => {
   return (
+    <>
     <header>
         <img className="logo-header" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/1200px-Airbnb_Logo_B%C3%A9lo.svg.png" alt="Logo AirBnb" />
+        <img className="logo-header-mini" src="https://cdn.iconscout.com/icon/free/png-256/free-airbnb-3770993-3147610.png?f=webp" alt="Logo AirBnb" />
         <ul className='header-buttons'>
             <li className='button-border'><strong>Qualquer lugar</strong></li>
             <li className='button-border'><strong>Qualquer semana</strong></li>
@@ -21,6 +23,20 @@ const Navbar = () => {
             <span className="material-symbols-outlined">account_circle</span>
         </div>
     </header>
+    <div className='baixe-app'>
+      <div className='baixe-app-div-logo'>
+      <span class="material-symbols-outlined">
+      close
+      </span>
+      <img className='logo-header-app' src="https://cdn.iconscout.com/icon/free/png-256/free-airbnb-logo-icon-download-in-svg-png-gif-file-formats--major-websites-set-pack-logos-icons-461772.png?f=webp&w=256" alt="airbnb logo" />
+      </div>
+      <div>
+        <h3>Use o aplicativo</h3>
+        <p>Acesso rápido e fácial ao Airbnb</p>
+      </div>
+      <button className='button-baixe-app'>ABRIR</button>
+    </div>
+    </>
   )
 }
 
